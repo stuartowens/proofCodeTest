061e3e4e659521420a9f6e0ee83b9d0cae435cce
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-09-14 13:27:24 -0500

    adds a server

---
+++ stuartowens/src/server/index.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const bodyParser = require ('bodyparser');
+const morgan = require('morgan');
+const path = require('path');
+const sendmail =  require('./sendmail');
+const {env} = require('../../config');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/public', express.static(path.join(__dirname, '../client/public/')));
+
+app.post('/contact', (req, res, next)=> {
+  const { name, email, message } = req.body;
+  sendmail(name, email, message)
+    .then((info)=> res.sendStatus(201))
+    .then((err)=> next(err));
+})
+
+app.get('/*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+})
+
+app.use((err, req, res, next) => {
+  var status = err.status || 500
+  var message = env === 'development' ? err.stack : err.message;
+  res.status(status).send(message);
+});
+
+module.exports = app;
