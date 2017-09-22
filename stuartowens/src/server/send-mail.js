3c91f811b9452b3c78847692fff32dc8401f8865
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-09-21 22:53:01 -0400

    adds send mailer

--- stuartowens/src/server/send-mail.js
+++ stuartowens/src/server/send-mail.js
@@ -13,5 +13,25 @@ const transporter = nodemailer.createTransport({
 });
 
 module.exports = (name, email, message ) => {
-  
+  name = escape(name);
+  email = escape(email);
+  message = escape(message);
+
+return new Promise((fulfill, reject) => {
+  transporter.sendMail(
+    {
+      from: `"${name}" <${email}>`
+      to: 'stuart@stuartowens.com',
+      subject: `${name} Message from stuartowens.com`,
+      html:
+      `<h2>You have recieved a message from ${name} at ${email}</h2>
+      <p>${message}</p>
+      `
+    },
+    (err, info) => {
+      if(err) { return reject(err); }
+      fulfill(info);
+    }
+  );
+})
 }
